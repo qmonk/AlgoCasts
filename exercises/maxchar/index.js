37e75eac8e3939fbe06e4e3a5e68e9c1b2313b19
qmonk <qmonk@protonmail.com>
2020-06-29 20:32:55 +0800

    Solve max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = [];
+    const map = {}; //char -> count
+
+    for (let i = 0; i < str.length; i++) {
+        const curr = str[i];
+        if (chars.indexOf(curr) >= 0) {
+            map[curr] = map[curr] + 1;
+        } else {
+            chars.push(curr);
+            map[curr] = 1;
+        }
+    }
+
+    let max = chars[0];
+    for (let i = 1; i < chars.length; i++) {
+        const curr = chars[i];
+        if (map[curr] > map[max]) {
+            max = curr;
+        }
+    }
+
+    return max;
+}
 
 module.exports = maxChar;
